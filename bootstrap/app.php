486ba3ffb0bdd55dad458b6187298fe3522b713d
Piotr Zwoliński <piotr.zwolinski6@gmail.com>
2018-06-30 11:23:18 +0000

    Merged in debugbar-fix (pull request #2)
    Poprawiony błąd z debugbarem

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,10 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-if (env('APP_DEBUG')) {
-    $app->register(Barryvdh\Debugbar\LumenServiceProvider::class);
-   }
-  // $app->configure('debugbar');
 /*
 |--------------------------------------------------------------------------
 | Return The Application
